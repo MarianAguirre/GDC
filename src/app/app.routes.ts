cc9dc5639132e150b3de6df9a34740145e186cb3
MarianAguirre <Marian.Aguirre.Carrill@gmail.com>
2024-11-29 13:26:07 -0600

    Ya funciona el navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { SeguridadComponent } from './seguridad';
+import { RefrigeracionComponent } from './refrigeracion';
+import { HardwareSoftwareComponent } from './hardware-software';
+import { DesignComponent } from './design';
+import { CostosComponent } from './costos';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'seguridad', component: SeguridadComponent },
+  { path: 'refrigeracion', component: RefrigeracionComponent },
+  { path: 'hardware', component: HardwareSoftwareComponent },
+  { path: 'design', component: DesignComponent },
+  { path: 'costos', component: CostosComponent },
+  { path: 'home', component: HomeComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' }
+
+];
