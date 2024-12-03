c7a2cedadfce9f3e2cea778fe42d265d922614f5
Marian Aguirre <Marian.Aguirre.Carrill@gmail.com>
2024-12-02 22:58:08 -0600

    Cambios al home

--- dist/gdc/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-ENXQ67O2.mjs";import"./chunk-AWUMHZVF.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { RefrigeracionComponent } from './refrigeracion';
 import { HardwareSoftwareComponent } from './hardware-software';
 import { DesignComponent } from './design';
 import { CostosComponent } from './costos';
-import { AppComponent } from './app.component';
 import { HomeComponent } from './home';
 
 export const routes: Routes = [
