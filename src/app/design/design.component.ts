203a1949b8874fa949e41802f5cc34e9f25ef9bd
MarianAguirre <Marian.Aguirre.Carrill@gmail.com>
2024-12-06 11:30:54 -0600

    fin de la zona de diseño

--- src/app/design/design.component.ts
+++ src/app/design/design.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-design',
@@ -8,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './design.component.css'
 })
 export class DesignComponent {
+  private scrollElements: HTMLElement[] = [];
 
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    // Selecciona todos los elementos con `data-scroll` dentro del componente
+    this.scrollElements = this.el.nativeElement.querySelectorAll('[data-scroll]');
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+
+    this.scrollElements.forEach((element) => {
+      const rect = element.getBoundingClientRect();
+
+      if (rect.top < windowHeight && rect.bottom > 0) {
+        // Agrega una clase para iniciar la animación solo una vez
+        if (!element.classList.contains('start-animation')) {
+          this.renderer.addClass(element, 'start-animation');
+        }
+      }
+    });
+  }
 }
